5c35825a4b9cb92e6e33f26c13ad9a873be6d4f6
Karen Makihara <karen_yoshimura@yahoo.com.br>
2020-10-22 15:35:35 -0300

    Tentei adicionar a célula de resumo da receita no detalhe da receita

--- Comidinhas/Cells/ResumoDaReceitaCell.swift
+++ Comidinhas/Cells/ResumoDaReceitaCell.swift
@@ -7,8 +7,16 @@
 
 import UIKit
 
-class ResumoDaReceitaCell: UITableViewCell {
 
+class ResumoDaReceitaCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var servingsLabel: UILabel!
+    
+    @IBOutlet weak var favoriteButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -18,6 +26,14 @@ class ResumoDaReceitaCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    
+    func setup(receita:Recipe) {
+        self.nameLabel.text = receita.name
+        self.timeLabel.text = receita.time
+        self.categoryLabel.text = receita.category
+        self.servingsLabel.text = receita.servings
     }
     
 }
--- Comidinhas/view/RecipeDetailVC.swift
+++ Comidinhas/view/RecipeDetailVC.swift
@@ -15,9 +15,45 @@ class RecipeDetailVC: UIViewController {
     var receita:Recipe?
     
 
+    private func configTableView() {
+        self.recipeDetailTableView.delegate = self
+        self.recipeDetailTableView.dataSource = self
+        
+        self.recipeDetailTableView.tableFooterView = UIView(frame: .zero)
+        
+        self.recipeDetailTableView.register(UINib(nibName: "ResumoDaReceitaCell", bundle: nil), forCellReuseIdentifier: "ResumoDaReceitaCell")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.configTableView()
 
+        
     }
 
+}
+
+
+
+extension RecipeDetailVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:ResumoDaReceitaCell? = self.recipeDetailTableView.dequeueReusableCell(withIdentifier: "ResumoDaReceitaCell", for: indexPath) as? ResumoDaReceitaCell
+        cell?.nameLabel.text = self.receita?.name
+        cell?.categoryLabel.text = self.receita?.category
+        cell?.timeLabel.text = self.receita?.time
+        cell?.servingsLabel.text = self.receita?.servings
+//        cell?.setup(receita: self.arrayreceitas[indexPath.row])
+        
+        return cell ?? UITableViewCell()
+    }
+    
+    
+    
+    
 }
