cd0dafb0b39c63fc173323f6c25ad9ebb297da22
Karen Makihara <karen_yoshimura@yahoo.com.br>
2020-10-21 22:47:54 -0300

    Celula de Resumo da Receita na tela de detalhes da receita

---
+++ Comidinhas/Cells/ResumoDaReceitaCell.swift
@@ -0,0 +1,23 @@
+//
+//  ResumoDaReceitaCell.swift
+//  Comidinhas
+//
+//  Created by Fabio Makihara on 21/10/20.
+//
+
+import UIKit
+
+class ResumoDaReceitaCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
