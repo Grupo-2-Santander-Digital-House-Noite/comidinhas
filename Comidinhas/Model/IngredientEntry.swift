ecdb87cfa2ad59db29ae6d2aeb9845e66c02188b
Rafael Nascimento Sampaio <ooredroxoo@gmail.com>
2020-10-23 02:07:01 -0300

    Adiciona modelo que representa entrada de ingrediente.

---
+++ Comidinhas/Model/IngredientEntry.swift
@@ -0,0 +1,25 @@
+//
+//  IngredientEntry.swift
+//  Comidinhas
+//
+//  Created by Rafael Nascimento Sampaio on 23/10/20.
+//
+
+import Foundation
+
+class IngredientEntry {
+    
+    /** Nome do Ingrediente **/
+    var name: String
+    /** Quantidade do Ingrediente **/
+    var quantity: Double
+    /** Unidade de medida **/
+    var measureUnity: String
+    
+    init(named name: String, withAmount quantity: Double, andMeasureUnity measureUnity: String) {
+        self.name = name
+        self.quantity = quantity
+        self.measureUnity = measureUnity
+    }
+    
+}
