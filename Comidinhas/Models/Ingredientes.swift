610dcf77aeb6bd25d5a6b812cfb2acaab2d42a82
Rafael Nascimento Sampaio <ooredroxoo@gmail.com>
2020-11-04 02:11:38 -0300

    Remove array de dados iniciais do struct de ingredientes.

--- Comidinhas/Models/Ingredientes.swift
+++ Comidinhas/Models/Ingredientes.swift
@@ -20,22 +20,3 @@ struct Ingredites : Codable {
         case ingrediente = "name"
     }
 }
-
-
-
-var arrayIngredientesDough:[Ingredites] = [Ingredites(quantidade: 2, unidade: "cup", ingrediente: "flour"),
-                                      Ingredites(quantidade: 2, unidade: "cup", ingrediente: "sugar"),
-                                      Ingredites(quantidade: 1, unidade: "cup", ingrediente: "milk"),
-                                      Ingredites(quantidade: 6, unidade: "tablespoon", ingrediente: "chocolate powder"),
-                                      Ingredites(quantidade: 1, unidade: "tablespoon", ingrediente: "baking powder"),
-                                      Ingredites(quantidade: 6, unidade: "unit", ingrediente: "egg"),
-                                      Ingredites(quantidade: 1, unidade: "tablespoon", ingrediente: "yeast")]
-
-
-var arrayIngredientesIcing:[Ingredites] = [Ingredites(quantidade: 2, unidade: "tablespoon", ingrediente: "butter"),
-                                           Ingredites(quantidade: 2, unidade: "cup", ingrediente: "milk"),
-                                           Ingredites(quantidade: 1, unidade: "cups", ingrediente: "chocolate powder")]
-
-
-var arrayPartesDaReceita:[String] = ["Dough", "Icing"]
-
