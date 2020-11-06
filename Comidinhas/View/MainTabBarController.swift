cfc2e24444c47ba5c2b7bc935d1a4001f7efebca
Rafael Nascimento Sampaio <ooredroxoo@gmail.com>
2020-11-06 00:04:10 -0300

    Adiciona comportamento para atualizar botão da lista de compra ao adicionar itens.

---
+++ Comidinhas/View/MainTabBarController.swift
@@ -0,0 +1,67 @@
+//
+//  MainTabBarController.swift
+//  Comidinhas
+//
+//  Created by Rafael Nascimento Sampaio on 05/11/20.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController, ShoppingListDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        ShoppingList.shared.subscribe(delegate: self)
+        
+    }
+    
+    private func getShoppingListTabBarItem() -> UITabBarItem? {
+        if let items = self.tabBar.items {
+            for item in items {
+                if(item.tag == 1000) {
+                    return item
+                }
+            }
+        }
+        
+        return nil
+    }
+    
+    // MARK: ShoppingListDelegate - Methods
+    func didAdd(_ shoppingList: ShoppingList, ingredient: IngredientEntry) {
+        updateShoppingListBadge()
+    }
+    
+    func didRemove(_ shoppingList: ShoppingList, ingredient: IngredientEntry) {
+        updateShoppingListBadge()
+    }
+    
+    private func updateShoppingListBadge() {
+        guard let item: UITabBarItem = self.getShoppingListTabBarItem() else {
+            return
+        }
+        
+        let quantidadeItens = ShoppingList.shared.getDesmarcados().count
+        
+        if quantidadeItens == 0 {
+            return
+        }
+        
+        item.badgeColor = .systemRed
+        item.badgeValue = "\(quantidadeItens < 10 ? "\(quantidadeItens)" : "9+")"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
